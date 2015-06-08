d7026e99195b30a25d1027e4bb939c8f600fc2e6
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2015-06-08 13:04:52 +0300

    Solved programming assignment for week2 question 3 and 4.

--- week2.py
+++ week2.py
@@ -1,7 +1,7 @@
 import binascii
 from builtins import zip, bytearray
 from Crypto.Cipher import AES
-
+from Crypto.Util import Counter
 
 def question4(out64, out32and32):
     out64bytes = binascii.a2b_hex(out64)
@@ -20,6 +20,8 @@ def question4_solve():
 def programming_assignment():
     pa_question1()
     pa_question2()
+    pa_question3()
+    pa_question4()
 
 def pa_question1():
     print("Question 1:")
@@ -29,7 +31,7 @@ def pa_question1():
 
     key = '140b41b22a29beb4061bda66b6747e14'
 
-    decode(key, ct_str)
+    decode_cbc(key, ct_str)
 
 def pa_question2():
     print("Question 2:")
@@ -39,9 +41,9 @@ def pa_question2():
 
     key = '140b41b22a29beb4061bda66b6747e14'
 
-    decode(key, ct_str)
+    decode_cbc(key, ct_str)
 
-def decode(key, ct_str):
+def decode_cbc(key, ct_str):
     iv = binascii.a2b_hex(ct_str[:32])
     ct = binascii.a2b_hex(ct_str[32:])
     pt = bytes()
@@ -56,3 +58,33 @@ def decode(key, ct_str):
     pt_wo_padding = pt[:-pt[-1]]
     print("Decoded message: '%s'" % pt_wo_padding.decode("ascii"))
 
+def pa_question3():
+    print("Question 3:")
+
+    ct_str = '69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc3' \
+             '88d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329'
+
+    key = '36f18357be4dbd77f050515c73fcf9f2'
+
+    decode_ctr(key, ct_str)
+
+def pa_question4():
+    print("Question 4:")
+
+    ct_str = '770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa' \
+             '0e311bde9d4e01726d3184c34451'
+
+    key = '36f18357be4dbd77f050515c73fcf9f2'
+
+    decode_ctr(key, ct_str)
+
+def decode_ctr(key, ct_str):
+    counter = Counter.new(128, initial_value=int.from_bytes(binascii.a2b_hex(ct_str[:32]), byteorder="big"))
+    ct = binascii.a2b_hex(ct_str[32:])
+    pt = bytes()
+    
+    for i in range((len(ct) + 15) // 16):
+        cipher = AES.new(binascii.a2b_hex(key), AES.MODE_CTR, counter=counter)
+        pt += cipher.decrypt(ct[i * 16 : (i + 1) * 16])
+
+    print("Decoded message: '%s'" % pt.decode("ascii"))
